a331180b477a18962d725e34d798323b3b6521e0
Aehtesham Ahmed <aehtesham2004@gmail.com>
2026-07-23 11:54:01 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,15 @@
+console.log(" Aehtesham Ahmed")
+
+//write a function to take two number and return
+//their sum
+function addNumbers(a, b) {
+    return a + b;
+}
+
+
+const add = (a ,b) => {
+    return a + b;
+};
+const add2 = (a,b) => a + b;
+
+co
